9870f350d61d2f1a8218ddca9d974ee929448d0f
fiston <kalambayifiston7@gmail.com>
2026-01-27 23:26:45 +0100

    customers page fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { QueryProvider } from '@/providers/query-provider';
 import '../styles/globals.css';
 import ConditionalNavbar from '@/components/ConditionalNavbar';
 import Footer from '@/components/Footer';
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -25,6 +26,7 @@ export default function RootLayout({
           <ConditionalNavbar />
           
           <main>{children}</main>
+          <Analytics />
           <footer>
             <Footer />
           </footer>
